8dcc784e6914f6b61a2f895cfa5dc194a1b10626
gferiancek <gferiancek@gmail.com>
2020-12-13 00:22:05 -0500

    Saving current work to test out LinearLayout Root instead of RelativeLayout

--- app/src/main/java/com/example/baseballtracker/MainActivity.java
+++ app/src/main/java/com/example/baseballtracker/MainActivity.java
@@ -60,28 +60,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addItemsToRecyclerViewArrayList(ArrayList<String> arrayList) {
-        arrayList.add(" 1");
-        arrayList.add(" 2");
-        arrayList.add(" 3");
-        arrayList.add(" 4");
-        arrayList.add(" 5");
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
         if (thirdRun) {
-            arrayList.add(" _");
-            arrayList.add(" 7");
-            arrayList.add(" _");
+            arrayList.add("_");
+            arrayList.add("7");
+            arrayList.add("_");
         } else {
-            arrayList.add(" 6");
-            arrayList.add(" 7");
-            arrayList.add(" 8");
+            arrayList.add("6");
+            arrayList.add("7");
+            arrayList.add("8");
         }
-        arrayList.add(" 9");
+        arrayList.add("9");
         arrayList.add("");
         if (firstRun) {
-            arrayList.add(" R");
-            arrayList.add(" H");
-            arrayList.add(" E");
+            arrayList.add("R");
+            arrayList.add("H");
+            arrayList.add("E");
         } else {
-            arrayList.add(" 8");
+            arrayList.add("8");
             arrayList.add("12");
             arrayList.add("13");
         }
